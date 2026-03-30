ed932f0579d44c1ce9e62679f4a544c418e00234
Poteau Laly <lalypoteau1@gmail.com>
2026-03-30 10:46:27 +0200

    side scroll working with all of the cards

--- src/Components/cards/cards.jsx
+++ src/Components/cards/cards.jsx
@@ -2,10 +2,22 @@ import jinshi_gif from "../../assets/gifs/jinshi.gif"
 import maomao_gif from "../../assets/gifs/maomao.gif"
 import maomao_card from "../../assets/maomao_chat_nouriture.png"
 import jinshi_card from "../../assets/jinshi_cheuveux_au_vent.png"
-import CardsIndividual from "../card_individual/card"
+import CardsIndividual from "../card_individual/CardIndividual"
 function Cards() {
 
     let cards = [
+        {
+            name: "Maomao",
+            image: maomao_card,
+            bg_gif: maomao_gif,
+            description: "Maomao (猫猫マオマオ, Maomao?) is the protagonist of The Apothecary Diaries series. She is an illegitimate member of the La Clan raised in the red-light district. One day, she got kidnapped and sold by bandits, and she suddenly found herself as a servant in the Imperial Palace of Li, where she used her sense of practicality, keen deductions, and insightful knowledge of herbs and illnesses to solve the mysterious problems within the Inner and Outer Courts."
+        },
+        {
+            name: "Jinshi",
+            image: jinshi_card,
+            bg_gif: jinshi_gif,
+            description: "Jinshi (壬氏ジンシ, Jinshi) is the deuteragonist of The Apothecary Diaries series. He appears to be a beautiful eunuch who was placed in charge of the Inner Court. He often requests for Maomao to solve mysteries in the Imperial Palace in exchange for rare medicines."
+        },
         {
             name: "Maomao",
             image: maomao_card,
@@ -35,8 +47,9 @@ function Cards() {
 
     return (
        
-        <div className="w-full min-h-screen gap-10 flex justify-center items-center p-4 md:p-8">
-             {cards.map(card => (<CardsIndividual {...card} />
+        <div className="w-full min-h-screen gap-10 flex overflow-x-auto items-center px-10 md:px-20 py-4 md:py-8 snap-x snap-mandatory">
+             {cards.map((card, index) => (
+                  <CardsIndividual key={index} {...card} />
              ))}
         </div>
     )
